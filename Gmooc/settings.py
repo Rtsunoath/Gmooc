941ebc293c818ba152c2d05073879c7120e0ff53
583708076@qq.com <583708076@qq.com>
2017-07-26 10:36:14 +0800

    [courses] add models (Course,Lesson,Video,CourseResource)

--- Gmooc/settings.py
+++ Gmooc/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'courses',
 ]
 
 AUTH_USER_MODEL = 'users.UserProfile'
